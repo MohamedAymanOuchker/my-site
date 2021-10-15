e9925a03b7cd87da6fe52575c1cd4827a73458a0
Mohamed Ayman Ouchker <ayman.ouchker3@gmail.com>
2021-10-15 20:37:51 +0100

    Add cloud animation and some responsive design updates.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,34 @@
+// Loader
+
 $(document).ready(function() {
-	
+
 	setTimeout(function(){
 		$('body').addClass('loaded');
 		$('h1').css('color','#222222');
 	}, 3000);
-	
+
+});
+
+// Animation
+
+$(document).ready(function(e) {
+	var width = $(document).width();
+
+	function goRight() {
+		$(".animate").animate({
+			left: width
+		}, 80000, function() {
+			setTimeout(goLeft, 50);
+		});
+	}
+
+	function goLeft() {
+		$(".animate").animate({
+			left: 0
+		}, 80000, function() {
+			setTimeout(goRight, 50);
+		});
+	}
+
+	setTimeout(goRight, 50);
 });
